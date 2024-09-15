9dd03ae36578b4e4d4d9efefb521b3dbd2c17e45
Vladimir Penzin <vladimir.penzin@dom.ru>
2024-09-16 00:37:42 +0500

    Фиксики CORS

--- server/web/build/constant.js
+++ server/web/build/constant.js
@@ -2,22 +2,23 @@ export const OUTPUT_DIR = 'dist'
 
 export const PROXY_CONFIG = {
   // /**
-  //  * @desc    Заменить совпадающие значения
-  //  * @Запрос путь  http://localhost:3100/api/user
-  //  * @Путь перенаправления  http://localhost:9999/api/v1 +/user
+  //  * @description    Заменить совпадающие значения
+  //  * @request_path   http://localhost:3100/api/user
+  //  * @redirect_path  http://localhost:9999/api/v1 +/user
   //  */
   // '/api': {
   //   target: 'http://localhost:9999/api/v1',
   //   changeOrigin: true,
   //   rewrite: (path) => path.replace(new RegExp('^/api'), ''),
   // },
+  
   /**
-   * @desc    Не заменять совпадающие значения
-   * @Запрос путь  http://localhost:3100/api/v1/user
-   * @Путь перенаправления  http://localhost:9999/api/v1/user
+   * @description     Не заменять совпадающие значения
+   * @request_path    http://localhost:3100/api/v1/user
+   * @redirect_path   http://localhost:9999/api/v1/user
    */
   '/api/v1': {
     target: 'http://127.0.0.1:9999',
-    changeOrigin: true,
+    changeOrigin: false,
   },
 }
