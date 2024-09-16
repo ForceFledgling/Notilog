e546a9cdd7aa73d456f5c5960b66e0643c29306e
Vladimir Penzin <vladimir.penzin@dom.ru>
2024-09-16 13:55:17 +0500

    Фиксики деплоя для Vercel

--- server/web/server.js
+++ server/web/server.js
@@ -5,17 +5,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3100;
 
-// Прокси-конфигурация для API
+// Прокси для API-запросов
 app.use('/api/v1', createProxyMiddleware({
   target: 'http://195.2.81.46:9999/api/v1',
   changeOrigin: true,
-  secure: false, // Разрешить использование http
+  secure: false
 }));
 
-// Обслуживание статических файлов из директории dist
+// Обслуживание статических файлов из папки dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Отдавать index.html для всех маршрутов, кроме API
+// Все запросы кроме API направляем на index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
